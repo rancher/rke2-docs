af238e37dfda89f0038a0e236196da7e5cad91b1
Derek Nola <derek.nola@suse.com>
2024-02-16 10:07:59 -0800

    Add page on server token rotation
    Signed-off-by: Derek Nola <derek.nola@suse.com>

--- sidebars.js
+++ sidebars.js
@@ -41,6 +41,7 @@ module.exports = {
         'security/selinux',
         'security/secrets_encryption',
         'security/certificates',
+        'security/token',
       ],
     },
     'architecture',
