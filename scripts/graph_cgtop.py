b8bbb2b547633caba7acc245447b1a50a5759282
Derek Nola <derek.nola@suse.com>
2024-05-01 09:09:16 -0700

    Cleanup comments on script
    Signed-off-by: Derek Nola <derek.nola@suse.com>

--- scripts/graph_cgtop.py
+++ scripts/graph_cgtop.py
@@ -1,3 +1,12 @@
+# Script used to parse the output of systemd-cgtop and generate a histogram of the memory usage
+# Used for resource profiling page in the RKE2 documentation
+# Generate input file using the following command:
+# systemd-cgtop system.slice/rke2-server.service --raw -b -n 1200 -d 250ms > systemd_cgtop_output.txt
+# Pulls data every 0.25s for 5 minutes
+
+# Data arragment is:
+# cgroup name, # of tasks, CPU %, MEM usage (bytes)
+
 # import matplotlib.pyplot as plt
 import re
 import numpy
@@ -7,12 +16,6 @@ cpu_usage = []
 memory_usage = []
 input_file = "systemd_cgtop_output.txt"
 cgroup = "rke2-server"
-# Generated input file using the following command:
-# Pulls data every 0.25s for 5 minute
-# systemd-cgtop system.slice/rke2-server.service --raw -b -n 1200 -d 250ms > systemd_cgtop_output.txt
-
-# Data arragment is:
-# cgroup name, # of tasks, CPU %, MEM usage (bytes)
 
 with open(input_file, 'r') as infile:
     # Iterate over each line in the input file
@@ -38,6 +41,8 @@ print(f'Number of Tasks: ', tasks_avg)
 print(f'95th Percentile CPU Usage: {cpu_95th:.2f}%')
 print(f'95th Percentile Memory Usage: {memory_95th:.2f} MB')
 
+# Optional Plotting
+# plt.hist(cpu_usage, bins=20, alpha=0.7, label='CPU Usage')
 # plt.hist(memory_usage, bins=20, alpha=0.7, label='Memory Usage')
 # plt.axvline(memory_95th, linestyle='dashed', linewidth=1, label=f'95th Percentile Memory ({memory_95th:.2f} MB)')
 
